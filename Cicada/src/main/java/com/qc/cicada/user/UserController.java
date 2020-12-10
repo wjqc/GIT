e8fe952f627f02a48cc9386212bd50bb188f33cc
wj <mxsa1995@163.com>
2020-12-10 17:29:00 +0800

    修改

--- Cicada/src/main/java/com/qc/cicada/user/UserController.java
+++ Cicada/src/main/java/com/qc/cicada/user/UserController.java
@@ -7,11 +7,13 @@ import com.qc.common.exception.ErrorCode;
 import com.qc.common.message.ApiResponse;
 import com.qc.common.message.SimpleRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 
@@ -26,9 +28,26 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/hello")
     public String hello(){
         return "Hello Wwold!";
+        //第一种调用方式
+        //String forObject = new RestTemplate().getForObject("http://localhost:8081/Hello/World?s=" + s, String.class);
+
+        //第二种调用方式
+        //根据服务名 获取服务列表 根据算法选取某个服务 并访问某个服务的网络位置。
+        //ServiceInstance serviceInstance = loadBalancerClient.choose("EUREKA-SERVICE");
+        //String forObject = new RestTemplate().getForObject("http://"+serviceInstance.getHost()+":"+serviceInstance.getPort()+"/Hello/World?s="+s,String.class);
+
+        //第三种调用方式 需要restTemplate注入的方式
+//        String forObject = restTemplate.getForObject("http://EUREKA-SERVICE/Hello/World?s=" + s, String.class);
+//        return forObject;
     }
 
     @RequestMapping("/login")
