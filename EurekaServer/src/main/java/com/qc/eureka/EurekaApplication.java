c45dc2ea5d4c1e69f1a85c8d5a09d412cc3e822a
wj <mxsa1995@163.com>
2020-12-11 09:36:07 +0800

    更换注册中心为nacos

--- EurekaServer/src/main/java/com/qc/eureka/EurekaApplication.java
+++ EurekaServer/src/main/java/com/qc/eureka/EurekaApplication.java
@@ -4,11 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @Slf4j
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class EurekaApplication {
 
     public static void main(String[] args) {
