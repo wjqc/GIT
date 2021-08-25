9c129713840a8861e2eb3629f934d73755ff0caf
wj <mxsa1995@163.com>
2021-08-25 12:00:39 +0800

    修改nacos配置

--- Cicada/src/main/java/com/qc/cicada/service/user/UserService.java
+++ Cicada/src/main/java/com/qc/cicada/service/user/UserService.java
@@ -4,15 +4,16 @@ import com.qc.common.bean.UserLoginBean;
 import com.qc.common.bean.UserRegisterBean;
 import com.qc.common.message.BaseResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = "Provider")
 public interface UserService {
 
-    @RequestMapping("/login")
+    @PostMapping("/login")
     BaseResponse login(@RequestBody UserLoginBean request);
 
-    @RequestMapping("/register")
+    @PostMapping("/register")
     BaseResponse register(@RequestBody UserRegisterBean bean);
 }
